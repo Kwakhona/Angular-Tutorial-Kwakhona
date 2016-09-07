dc50cc5f9345ab73623d38a4a528fe9589c5a057
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-07 13:23:34 +0200

    Updated create project feature

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -8,23 +8,23 @@
  * Controller of the angularTutorialKwakhonaApp
  */
 angular.module('angularTutorialKwakhonaApp')
-    .controller('ProjectCtrl', function($scope, $window, $route, projectService) {
+    .controller('ProjectCtrl', function ($scope, $window, $route, projectService) {
         // get all projects
         projectService.getProjects()
-            .then(function(response){
+            .then(function (response) {
                 $scope.projects = response.data;
             })
-            .catch(function(error){
-                $window.alert("Error: "+ error);
+            .catch(function (error) {
+                $window.alert("Error: " + error);
             });
 
         // updating add/edit form
-        $scope.UpdateForm = function(project){
+        $scope.UpdateForm = function (project) {
             $scope.form = { 'edited': false, 'added': false };
 
-            if(project !== null){
+            if (project !== null) {
                 $scope.project = {
-                    'pk':project.pk,
+                    'pk': project.pk,
                     'title': project.title,
                     'description': project.description,
                     'start_date': project.start_date,
@@ -39,48 +39,126 @@ angular.module('angularTutorialKwakhonaApp')
                     'description': '',
                     'start_date': '',
                     'end_date': '',
-                    'is_billable': '',
-                    'is_active': ''
+                    'is_billable': false,
+                    'is_active': false
                 };
             }
 
         };
-        
+
+        $scope.changeMonth = function (month) {
+            switch (month) {
+                case "Jan":
+                    month = "01";
+                    break;
+                case "Feb":
+                    month = "02";
+                    break;
+                case "Mar":
+                    month = "03";
+                    break;
+                case "Apr":
+                    month = "04";
+                    break;
+                case "May":
+                    month = "05";
+                    break;
+                case "Jun":
+                    month = "06";
+                    break;
+                case "Jul":
+                    month = "07";
+                    break;
+                case "Aug":
+                    month = "08";
+                    break;
+                case "Sep":
+                    month = "09";
+                    break;
+                case "Oct":
+                    month = "10";
+                    break;
+                case "Nov":
+                    month = "11";
+                    break;
+                case "Dec":
+                    month = "12";
+                    break;
+            }
+            return month;
+        };
+
         // creating a project
-        $scope.addProject = function(){
-            $scope.project.pk = "";
+        $scope.addProject = function () {
+            var day, month, year;
+
+            if (angular.isDefined($scope.project.start_date)) {
+                day = $scope.project.start_date.toString().substr(8, 2);
+                month = $scope.project.start_date.toString().substr(4, 3);
+                year = $scope.project.start_date.toString().substr(11, 4);
+
+                month = $scope.changeMonth(month);
+                $scope.project.start_date = year + "-" + month + "-" + day;
+            }
+
+            if (angular.isDefined($scope.project.end_date)) {
+                day = $scope.project.end_date.toString().substr(8, 2);
+                month = $scope.project.end_date.toString().substr(4, 3);
+                year = $scope.project.end_date.toString().substr(11, 4);
+
+                month = $scope.changeMonth(month);
+                $scope.project.end_date = year + "-" + month + "-" + day;
+            }
+
+
             projectService.createProject($scope.project)
-                .then(function(){
+                .then(function() {
                     $scope.form = { 'added': true };
                 })
-                .catch(function(error){
-                    $window.alert("Error: "+ JSON.stringify(error));
+                .catch(function (error) {
+                    if (angular.isDefined(error.data)) {
+                        var err = "ERRORS \n";
+                        if (angular.isDefined(error.data.title)) {
+                            err += "Title: " + error.data.title + "\n";
+                        }
+                        if (angular.isDefined(error.data.description)) {
+                            err += "Description: " + error.data.description + "\n";
+                        }
+                        if (angular.isDefined(error.data.start_date)) {
+                            err += "Start Date: " + error.data.start_date + "\n";
+                        }
+                        if (angular.isDefined(error.data.end_date)) {
+                            err += "End Date: " + error.data.end_date + "\n";
+                        }
+                        $window.alert(err);
+                    }
                 });
         };
-        
+
         // updating a project
-        $scope.updateProject = function(){
+        $scope.updateProject = function () {
+            console.log($scope.project);
             projectService.updateProject($scope.project.pk, $scope.project)
-                .then(function(){
+                .then(function () {
                     $scope.form = { 'edited': true };
                 })
-                .catch(function(error){
-                    $window.alert("Error: "+ error);
+                .catch(function (error) {
+                    console.log("Error: " + error);
                 });
         };
 
         // deleting a project
-        $scope.deleteProject = function(project){
-            if($window.confirm("Are you sure you want to delete project: "+ project.title) === true){
+        $scope.deleteProject = function (project) {
+            if ($window.confirm("Are you sure you want to delete project: " + project.title) === true) {
                 project.deleteProject(project.pk)
-                    .then(function(){
+                    .then(function () {
 
                     })
-                    .catch(function(error){
-                        $window.alert("Error: "+ error);
+                    .catch(function (error) {
+                        $window.alert("Error: " + error);
                     });
             } else {
-                $window.alert("You have cancelled the deletion off project: "+ project.title);
+                $window.alert("You have cancelled the deletion off project: " + project.title);
             }
-        } ;
+        };
     });
