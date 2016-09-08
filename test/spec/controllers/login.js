f6714a08f7a85070971c541b21472123a762252b
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-08 14:54:47 +0200

    updated login uni test

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -6,30 +6,19 @@ describe('Controller: LoginCtrl', function () {
     beforeEach(module('angularTutorialKwakhonaApp'));
 
     var LoginCtrl,
-        $scope,
-        location,
-        authService;
+        $scope;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function ($controller, $rootScope, $location, UserAuthentication) {
+    beforeEach(inject(function ($controller, $rootScope) {
         $scope = $rootScope.$new();
         LoginCtrl = $controller('LoginCtrl', {
             $scope: $scope
         });
 
-        location = $location;
-        authService = UserAuthentication;
-
-        $scope = { username: 'admin', password: 'admin' };
     }));
 
-    it('should redirect to project after Login()', function () {
-        $scope.Login = authService.login($scope.username, $scope.password)
-                        .then(function(response){
-                            expect(response.token).toBe('71456dbd15de0c0b6d2b4b44e5a92ad94c6def97');
-                            location.path('/projects');
-                        });
-
+    it('should initialize the LoginCtrl', function () {
+        expect(LoginCtrl).toBeDefined();
     });
 
 });
