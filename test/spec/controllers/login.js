3eb43d245cdecffb50b45905145154f5edbc2cea
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-08 07:40:13 +0200

    updated unit test for login ctrl

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -24,15 +24,20 @@ describe('Controller: LoginCtrl', function () {
         location = $location;
         uri = _AUTH_SERVICE_BASE_URI_ + 'api-token-auth/';
 
-        $scope = { username : 'admin', password: 'admin' };
+        $scope = { username: 'admin', password: 'admin' };
     }));
 
-    it('should fire the Login() function', function(){
+    it('should login in successfully', function () {
 
-    });
-    it('should redirect after Login() is called', function(){
-        
+        httpBackend.expect('POST', uri)
+            .respond(200, "{token: 71456dbd15de0c0b6d2b4b44e5a92ad94c6def97}");
 
+        _authService.login($scope.username, $scope.password)
+            .then(function (data) {
+                expect(data.token).toBe('71456dbd15de0c0b6d2b4b44e5a92ad94c6def97');
+            });
+
+        httpBackend.flush();
     });
 
 });
