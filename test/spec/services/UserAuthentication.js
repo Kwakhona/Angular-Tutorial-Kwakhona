84a6cfb71bdff90e5354d02a707d0d72ac0805ab
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-08 15:37:34 +0200

    updated user authentication unit test

--- test/spec/services/UserAuthentication.js
+++ test/spec/services/UserAuthentication.js
@@ -8,23 +8,38 @@ describe('Service: UserAuthentication', function () {
     var authService,
         scope,
         httpBackend,
+        cookie,
         url;
 
     // Initialize the controller and a mock scope
-    beforeEach(inject(function (UserAuthentication, $httpBackend, AUTH_SERVICE_BASE_URI) {
+    beforeEach(inject(function (UserAuthentication, $httpBackend, $cookie, AUTH_SERVICE_BASE_URI) {
         url = AUTH_SERVICE_BASE_URI + 'api-token-auth/';
         authService = UserAuthentication;
         httpBackend =$httpBackend;
         scope = {username: 'admin', password: 'admin'};
+        cookie = $cookie;
     }));
 
-    it('should get login success', function () {
+    it('should get token on successful authentication', function () {
         httpBackend.expect('POST', url)
             .respond(200, { 'token': '71456dbd15de0c0b6d2b4b44e5a92ad94c6def97' });
 
         authService.login(scope.username, scope.password)
             .then(function (data) {
-                expect(data.token).toBe('71456dbd15de0c0b6d2b4b44e5a92ad94c6def97');
+                expect(!data.token).toBe(true);
+                expect(cookie.get('token')).toBe('71456dbd15de0c0b6d2b4b44e5a92ad94c6def97');
+            });
+
+        httpBackend.flush();
+    });
+
+    it('should get error message on authentication failure', function () {
+        httpBackend.expect('POST', url)
+            .respond(200, {"non_field_errors": ["Unable to login with provided credentials."] });
+
+        authService.login(scope.username, scope.password)
+            .then(function (data) {
+                expect(!data.non_field_errors).toBe('Unable to login with provided credentials.');
             });
 
         httpBackend.flush();
