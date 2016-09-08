81264918f0053557c6f59dade2908a87c2de47ea
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-08 15:06:05 +0200

    updated user authenication unit test

--- test/spec/services/UserAuthentication.js
+++ test/spec/services/UserAuthentication.js
@@ -1,23 +1,32 @@
-// 'use strict';
+'use strict';
 
-// describe('Service: UserAuthentication', function () {
+describe('Service: UserAuthentication', function () {
 
-//   // load the controller's module
-//   beforeEach(module('angularTutorialKwakhonaApp'));
+    // load the controller's module
+    beforeEach(module('angularTutorialKwakhonaApp'));
 
-//   var authService,
-//     scope;
+    var authService,
+        scope,
+        httpBackend,
+        url;
 
-//   // Initialize the controller and a mock scope
-//   beforeEach(inject(function ($controller, $rootScope) {
-//     scope = $rootScope.$new();
-//     authCtrl = $controller('MainCtrl', {
-//       $scope: scope
-//       // place here mocked dependencies
-//     });
-//   }));
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function (UserAuthentication, $httpBackend, AUTH_SERVICE_BASE_URI) {
+        url = AUTH_SERVICE_BASE_URI + 'api-token-auth/';
+        authService = UserAuthentication;
+        httpBackend =$httpBackend;
+        scope = {username: 'admin', password: 'admin'};
+    }));
 
-//   it('should attach a list of awesomeThings to the scope', function () {
-//     expect(MainCtrl.awesomeThings.length).toBe(3);
-//   });
-// });
+    it('should get login success', function () {
+        httpBackend.expect('POST', url)
+            .respond(200, { 'token': '71456dbd15de0c0b6d2b4b44e5a92ad94c6def97' });
+
+        authService.login(scope.username, scope.password)
+            .then(function (data) {
+                expect(data.token).toBe('71456dbd15de0c0b6d2b4b44e5a92ad94c6def97');
+            });
+
+        httpBackend.flush();
+    });
+});
